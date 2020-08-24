eda70dfabc8ea4e3c7a6f2a53b22e77150cd1c0c
Sai Prashanth T S <saiprashanthts@gmail.com>
2020-08-24 08:43:48 +0530

    Added Argparse and parameters for the script
    Signed-off-by: Sai Prashanth T S <saiprashanthts@gmail.com>

--- Reading_Writing_Data_into_S3.py
+++ Reading_Writing_Data_into_S3.py
@@ -17,24 +17,46 @@ except Exception as e:
 
 class S3Load:
 
-    def __init__(self):
+    def __init__(self, bucket_name='saitestts'):
+        self.bucket_name = bucket_name
+
+    def read_Data(self, file_name = 'saitestts'):
         pass
 
-    def read_Data(self):
-        pass
-
-    def write_data(self):
+    def write_data(self, src_file_name = '', target_file_name = ''):
         pass
 
     def list_files(self):
         pass
 
-    def delete_file(self):
+    def delete_file(self, file_name = 'OnlineRetail.csv'):
         pass
 
-def main():
-    pass
+
+def main(bucket_name, choice):
+    load_obj = S3Load(bucket_name=bucket_name)
+    if choice == "LIST":
+        load_obj.list_files()
+    elif choice == "DELETE":
+        load_obj.delete_file()
+    elif choice == "READ":
+        load_obj.read_Data()
+    elif choice == "WRITE":
+        load_obj.write_data()
+    else:
+        print("Invalid Choice")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="To Load and Read Data present in S3",
+                                     prog="Reading_Writing_Data_into_S3.py")
+    parser.add_argument("--choice","-choice",required=True, choices= ['DELETE', 'READ', 'WRITE', 'LIST'],
+                        default='READ',dest="choice"
+                        )
+    parser.add_argument("--bucket_name", "-bucket_name", required= False, default= "saitestts",
+                        dest="bucket")
+    arg = parser.parse_args()
+    choice = arg.choice
+    choice = choice.upper()
+    bucket_name = arg.bucket
+    main(bucket_name, choice)
