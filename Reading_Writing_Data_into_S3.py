bd96d1a437063cbc541d8552e11377c5bc9ee20d
Sai Prashanth T S <saiprashanthts@gmail.com>
2020-08-24 09:17:20 +0530

    added delete, List and Read Utility
    Signed-off-by: Sai Prashanth T S <saiprashanthts@gmail.com>

--- Reading_Writing_Data_into_S3.py
+++ Reading_Writing_Data_into_S3.py
@@ -11,36 +11,64 @@ try:
     import boto3
     from io import StringIO
     import argparse
+    import csv
 except Exception as e:
-    print("SOme packages are missing. Please install them and then continue. Error Message is {}".format(e))
+    print("Some packages are missing. Please install them and then continue. Error Message is {}".format(e))
 
 
 class S3Load:
 
     def __init__(self, bucket_name='saitestts'):
         self.bucket_name = bucket_name
+        self.resources = boto3.resource('s3')
+        self.client = boto3.client('s3')
 
-    def read_Data(self, file_name = 'saitestts'):
-        pass
+    def read_data(self, file_name='Values.txt'):
+        data_list = []
+        response = self.client.get_object(
+            Bucket = self.bucket_name,
+            Key = file_name
+        )
+        data = response['Body'].read().decode('utf-8')
+        data = csv.reader(data.split('\r\n'))
+        header = next(data)
+        for row in data:
+            data_list.append(row)
+        df = pd.DataFrame(data=data_list, columns= header)
+        return df
 
-    def write_data(self, src_file_name = '', target_file_name = ''):
-        pass
+    def write_data(self, src_file_name='', target_file_name=''):
+        self.client.put_object
 
     def list_files(self):
-        pass
+        """
+        UDF to list the files of the given Bucket
+        :return: List of files
+        """
+        response = self.client.list_objects(
+            Bucket = self.bucket_name
+        )
+        return [(i['Key']) for i in response['Contents']]
 
-    def delete_file(self, file_name = 'OnlineRetail.csv'):
-        pass
+
+    def delete_file(self, file_name='OnlineRetail.csv'):
+        response = self.client.delete_object(
+            Bucket =self.bucket_name,
+            Key = file_name
+        )
+        return (response)
 
 
 def main(bucket_name, choice):
     load_obj = S3Load(bucket_name=bucket_name)
     if choice == "LIST":
-        load_obj.list_files()
+        files = load_obj.list_files()
+        print(files)
     elif choice == "DELETE":
         load_obj.delete_file()
     elif choice == "READ":
-        load_obj.read_Data()
+        df = load_obj.read_data()
+        print(df.head(20))
     elif choice == "WRITE":
         load_obj.write_data()
     else:
